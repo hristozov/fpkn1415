5cc5b26734c5fa6c81ea74d814d5821a9518fd4a
Georgi Hristozov <georgi@forkbomb.nl>
2014-10-05 00:34:56 +0300

    Added "07pow"

---
+++ exercises/01/07pow.scm
@@ -0,0 +1,13 @@
+(load "../../lib/unit.scm")
+
+(define (pow x n)
+  (if (= 0 n)
+      1
+      (* x (pow x (- n 1)))))
+
+(assert= 1 (pow 2 0))
+(assert= 2 (pow 2 1))
+(assert= 4 (pow 2 2))
+(assert= 1024 (pow 2 10))
+(assert= 1024 (pow -2 10))
+(assert= -2048 (pow -2 11))
