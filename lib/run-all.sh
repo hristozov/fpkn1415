2e378019951db9141cec8e37ee47192b262d1891
Georgi Hristozov <georgi@forkbomb.nl>
2014-10-04 22:18:16 +0300

    Some colors to the test runner.

--- lib/run-all.sh
+++ lib/run-all.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+RED=`echo '\033[41m\033[37m'`
+GREEN=`echo '\033[42m\033[37m'`
+WHITE=`echo '\033[47m\033[37m'`
+NORMAL=`echo '\033[0m'`
+
 for file in *.scm; do 
-	echo $file
-	plt-r5rs $file
+	echo "${WHITE}${file}${NORMAL}"
+	plt-r5rs $file | sed "s/FAILED/${RED}FAILED${NORMAL}/g" | \
+		sed "s/PASSED/${GREEN}PASSED${NORMAL}/g"
 	echo
 done
