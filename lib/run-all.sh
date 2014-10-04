eb2a3f79d17bd84c6a2d9ab9a7c16223dba4e884
Georgi Hristozov <georgi@forkbomb.nl>
2014-10-05 00:25:13 +0300

    Test output improvements.

--- lib/run-all.sh
+++ lib/run-all.sh
@@ -1,13 +1,38 @@
 #!/bin/sh
 
+set -e
+
 RED=`echo '\033[41m\033[37m'`
 GREEN=`echo '\033[42m\033[37m'`
 WHITE=`echo '\033[47m\033[37m'`
 NORMAL=`echo '\033[0m'`
 
-for file in *.scm; do 
+num_files=0
+num_passed=0
+num_failed=0
+num_total=0
+
+for file in *.scm; do
 	echo "${WHITE}${file}${NORMAL}"
-	plt-r5rs $file | sed "s/FAILED/${RED}FAILED${NORMAL}/g" | \
+	output=`mktemp -t scmtest`
+
+	plt-r5rs $file > $output
+
+	num_passed_local=`cat $output | grep '^PASSED' | wc -l`
+	num_failed_local=`cat $output | grep '^FAILED' | wc -l`
+	num_total_local=$((num_passed_local + num_failed_local))
+	num_passed=$((num_passed + num_passed_local))
+	num_failed=$((num_failed + num_failed_local))
+	num_total=$((num_total + num_total_local))
+	num_files=$((num_files + 1))
+
+	cat $output | sed "s/FAILED/${RED}FAILED${NORMAL}/g" | \
 		sed "s/PASSED/${GREEN}PASSED${NORMAL}/g"
+
+	rm $output
+
+	# newline after every file
 	echo
 done
+
+echo "$num_files files with $num_total tests: $num_passed passed and $num_failed failed"
