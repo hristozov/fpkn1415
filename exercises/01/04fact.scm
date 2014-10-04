3adec3c9ea7d942be34028cfb3bfa847542cc5dc
Georgi Hristozov <georgi@forkbomb.nl>
2014-10-04 22:23:41 +0300

    Added some more examples.

---
+++ exercises/01/04fact.scm
@@ -0,0 +1,9 @@
+(load "../../lib/unit.scm")
+
+(define (fact n)
+	(if (<= n 1)
+		1
+		(* n (fact (- n 1)))))
+
+(assert= 1 (fact 1))
+(assert= 24 (fact 4))
---
+++ exercises/01/05sum.scm
@@ -0,0 +1,10 @@
+(load "../../lib/unit.scm")
+
+(define (sum start end)
+  (if (>= start end)
+      end
+      (+ start (sum (+ start 1) end))))
+
+(assert= 4 (sum 4 4))
+(assert= 15 (sum 1 5))
+(assert= 5050 (sum 1 100))
