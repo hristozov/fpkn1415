6d286f164d067eac8da00b57fd28d1177c7d551c
Georgi Hristozov <georgi@forkbomb.nl>
2014-10-07 17:38:40 +0300

    Minor: Removed parentheses.

--- lib/hs/unit.hs
+++ lib/hs/unit.hs
@@ -1,12 +1,12 @@
 module Unit where
 
 assert :: Bool -> String -> IO ()
-assert False message = putStrLn ("FAILED: " ++ message)
-assert True  message = putStrLn ("PASSED: " ++ message)
+assert False message = putStrLn $ "FAILED: " ++ message
+assert True  message = putStrLn $ "PASSED: " ++ message
 
 assertEqual :: (Show a, Eq a) => a ->  a -> IO ()
 assertEqual expected actual =
-	assert (expected == actual) (show expected ++ "==" ++ show actual)
+	assert (expected == actual) $ show expected ++ " == " ++ show actual
 
 assertTrue :: Bool -> IO ()
 assertTrue actual =
