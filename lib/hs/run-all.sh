cc75b59757f3a3c50972ca92bc46be0c1f047750
Georgi Hristozov <georgi@forkbomb.nl>
2014-10-07 17:26:58 +0300

    More eyecandy.

--- lib/hs/run-all.sh
+++ lib/hs/run-all.sh
@@ -4,30 +4,18 @@ set -e
 
 . $(dirname $0)/../shared/runner.sh
 
-start=`date +%s`
-
 for file in *.hs; do
 	printFileHeader $file
-	
+
 	output=`mktemp -t scmtest`
 
-	# XXX: this relative path is a real disaster
-	currentDir=$(dirname $0)
-	searchPath="$currentDir"
-	echo $searchPath
-	ghc -i$currentDir $file -o a.out
+	ghc -i$(dirname $0) $file -o a.out
 	./a.out > $output
+
 	printFile $file $output
 
 	rm $output
 	rm a.out
-
-	# newline after every file
-	echo
 done
 
-end=`date +%s`
-runtime=$((end-start))
-
-printSummary $num_files $num_passed $num_failed $runtime
-
+printSummary
--- lib/shared/runner.sh
+++ lib/shared/runner.sh
@@ -6,8 +6,9 @@ GREEN_BACKGROUND=`tput setab 2`
 WHITE_BACKGROUND=`tput setab 7`
 NORMAL_COLORS=`tput sgr 0`
 
-function printFileHeader ()
-{
+startTime=`date +%s`
+
+function printFileHeader () {
 	file=$1
 	echo "${WHITE_BACKGROUND}${BLACK_FOREGROUND}${file}${NORMAL_COLORS}"
 }
@@ -27,14 +28,16 @@ function printFile () {
 	cat $output | \
 		sed "s/FAILED/${RED_BACKGROUND}${BLACK_FOREGROUND}FAILED${NORMAL_COLORS}/g" | \
 		sed "s/PASSED/${GREEN_BACKGROUND}${BLACK_FOREGROUND}PASSED${NORMAL_COLORS}/g"
+
+	# newline after every file
+	echo
 }
 
 function printSummary() {
-	num_files=$1
-	num_passed=$2
-	num_failed=$3
 	num_total=$((num_passed + num_failed))
-	runtime=$4
+	
+	end=`date +%s`
+	runtime=$((end-startTime))
 
 	if [ $num_failed -gt 0 ]
 		then
@@ -42,8 +45,20 @@ function printSummary() {
 	else
 		echo "${GREEN_BACKGROUND}"
 	fi
-	echo "${BLACK_FOREGROUND}$num_files files: $num_passed/$num_total passed; \
-took $runtime seconds${NORMAL_COLORS}"
+
+	if [ $num_files -ne 1 ]
+		then
+		files_suffix="s"
+	fi
+
+	if [ $runtime -ne 1 ]
+		then
+		seconds_suffix="s"
+	fi
+
+
+	echo "${BLACK_FOREGROUND}$num_files file$files_suffix: $num_passed/$num_total passed; \
+took $runtime second$seconds_suffix${NORMAL_COLORS}"
 }
 
 export -f printFileHeader
