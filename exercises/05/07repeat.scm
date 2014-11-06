7b13bed6aa7f86d097d5fae1538bd5de33aa9f9d
Georgi Hristozov <georgi@forkbomb.nl>
2014-11-07 00:29:53 +0200

    Add two more tasks.

--- exercises/05/07repeat.scm
+++ exercises/05/07repeat.scm
@@ -1,12 +1,12 @@
 (load "../../lib/scm/unit.scm")
 (load "05composition.scm")
 
-(define (repeat-n f n)
+(define (repeat-n-recur f n)
   (if (= n 0)
       (lambda (x) x)
-      (compose f (repeat-n f (- n 1)))))
+      (compose f (repeat-n-recur f (- n 1)))))
 
-(define (repeat-n-iter f n)
+(define (repeat-n f n)
   (define (helper result i)
     (if (= i 0)
         result
@@ -15,5 +15,5 @@
 
 (define (plus1 x) (+ x 1))
 
+(assert= 6 ((repeat-n-recur plus1 5) 1))
 (assert= 6 ((repeat-n plus1 5) 1))
-(assert= 6 ((repeat-n-iter plus1 5) 1))
