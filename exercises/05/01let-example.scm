3c3446e3e749d990b757d4a018feaa9a16f89342
Georgi Hristozov <georgi@forkbomb.nl>
2014-11-07 09:23:40 +0200

    Formatting. Add a README.

--- exercises/05/01let-example.scm
+++ exercises/05/01let-example.scm
@@ -6,11 +6,12 @@
      (x-plus-5 (+ x 5)))
     (* x-plus-3 x-plus-5)))
 
+; Долното няма да работи с let, тъй като x-plus-3 няма да е дефинирано. 
 (define (testlet* x)
   (let*
     ((x-plus-3 (+ x 3))
      (x-plus-5 (+ x-plus-3 2)))
-  (* x-plus-3 x-plus-5)))
+    (* x-plus-3 x-plus-5)))
 
 (assert= 35 (testlet 2))
 (assert= 35 (testlet* 2))
