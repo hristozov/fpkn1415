f7c9ee9e100f33b22139b4da7da5c00b74d553b4
Georgi Hristozov <georgi@forkbomb.nl>
2014-11-14 11:18:15 +0200

    Added an accumulate-based version of repeat.

--- exercises/06/02repeat.scm
+++ exercises/06/02repeat.scm
@@ -1,5 +1,6 @@
 (load "../../lib/scm/unit.scm")
 (load "01composition.scm")
+(load "../04/04accumulate.scm")
 
 (define (repeat-n-recur f n)
   (if (= n 0)
@@ -13,7 +14,16 @@
         (helper (compose f result) (- i 1))))
   (helper (lambda (x) x) n))
 
+(define (repeat-accumulate f n)
+  (accumulate (lambda (_) f)
+              compose
+              (lambda (x) x)
+              1
+              plus1
+              n))
+
 (define (plus1 x) (+ x 1))
 
 (assert= 6 ((repeat-n-recur plus1 5) 1))
 (assert= 6 ((repeat-n plus1 5) 1))
+(assert= 6 ((repeat-accumulate plus1 5) 1))
