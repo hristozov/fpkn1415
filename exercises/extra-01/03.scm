6316d1144b96fd07dfae9d8eccc9b98165881f2f
Georgi Hristozov <georgi@forkbomb.nl>
2014-11-15 15:31:59 +0200

    Code style.

--- exercises/extra-01/03.scm
+++ exercises/extra-01/03.scm
@@ -9,30 +9,13 @@
 
 (define (left-side m n)
   (define (term i)
-    (/ 1
-       (accumulate (lambda (j)
-                     (+ i j))
-                   *
-                   1
-                   0
-                   plus1
-                   m)))
-  (accumulate term
-              +
-              0
-              1
-              plus1
-              n))
+    (/ 1 (accumulate (lambda (j) (+ i j)) * 1 0 plus1 m)))
+  (accumulate term + 0 1 plus1 n))
+
 (define (right-side m n)
   (* (/ 1 m)
      (- (/ 1 (fact m))
-        (/ 1 (accumulate (lambda (i)
-                           (+ n i))
-                         *
-                         1
-                         1
-                         plus1
-                         m)))))
+        (/ 1 (accumulate (lambda (i) (+ n i)) * 1 1 plus1 m)))))
 
 (define (check m n)
   (= (left-side m n)
