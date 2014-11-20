27d5e942a702719f9b8af28b7cb4e997155a25bb
Georgi Hristozov <georgi@forkbomb.nl>
2014-11-20 22:57:39 +0200

    More solutions.

---
+++ tests/01/02_04.scm
@@ -0,0 +1,39 @@
+(load "../../lib/scm/unit.scm")
+(load "../../exercises/04/04accumulate.scm")
+
+(define (derive f)
+  (lambda (x)
+    (let ((x0 (+ x 0.001)))
+      (/ (- (f x) (f x0))
+         (- x x0)))))
+
+(define (compose f g)
+  (lambda (x)
+    (g (f x))))
+
+(define (repeat-n f n)
+  (define (helper result i)
+    (if (= i 0)
+        result
+        (helper (compose f result) (- i 1))))
+  (helper (lambda (x) x) n))
+
+(define (derive-n f n)
+  ((repeat-n derive n) f))
+
+(define (solution m n f g)
+  (lambda (x)
+    (accumulate (lambda (k)
+                  ((repeat-n f k)
+                   (accumulate (lambda (p)
+                                 ((derive-n g p) x))
+                               + 0 1 plus1 n)))
+                * 1 1 plus1 m)))
+
+(assert-approx 17554
+               35
+               ((solution 2
+                          3
+                          (lambda (x) (+ x 3))
+                          (lambda (y) (expt y 4)))
+                2))
