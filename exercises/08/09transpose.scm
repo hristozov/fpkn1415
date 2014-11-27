4c462f571b1efc289246ab178a1a45b777ba41ad
Georgi Hristozov <georgi@forkbomb.nl>
2014-11-28 00:31:11 +0200

    Add transpose.

---
+++ exercises/08/09transpose.scm
@@ -0,0 +1,17 @@
+(load "../../lib/scm/unit.scm")
+(load "../06/06nth.scm")
+
+(define (get-nth-row m n)
+  (map (lambda (row) 
+         (nth row n)) 
+       m))
+
+(define (transpose m)
+  (define (helper i)
+    (if (= i (length m))
+        '()
+        (cons (get-nth-row m i)
+              (helper (+ i 1)))))
+  (helper 0))
+
+(assert-equal '((1 4 7) (2 5 8) (3 6 9)) (transpose '((1 2 3) (4 5 6) (7 8 9))))
