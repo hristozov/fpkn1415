2ab8d255e0ef3a9dd7091091b8a61f4fda1941a1
Georgi Hristozov <georgi@forkbomb.nl>
2014-12-05 09:57:39 +0200

    More improvements.

--- exercises/09/04transpose.scm
+++ exercises/09/04transpose.scm
@@ -14,4 +14,9 @@
               (helper (+ i 1)))))
   (helper 0))
 
+(define (transpose2 m)
+  (apply map list m))
+
+
 (assert-equal '((1 4 7) (2 5 8) (3 6 9)) (transpose '((1 2 3) (4 5 6) (7 8 9))))
+(assert-equal '((1 4 7) (2 5 8) (3 6 9)) (transpose2 '((1 2 3) (4 5 6) (7 8 9))))
