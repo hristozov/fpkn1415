4819dd22b5c34d9f03fb8cad1575fcbf10306511
Georgi Hristozov <georgi@forkbomb.nl>
2014-12-05 08:59:41 +0200

    Improvements.

--- exercises/09/01diagonal.scm
+++ exercises/09/01diagonal.scm
@@ -9,6 +9,22 @@
               (helper (+ i 1)))))
   (helper 0))
 
+(define (diagonal2 m)
+  (if (null? m)
+      '()
+      (cons (caar m)
+            (diagonal2 (cdr (map cdr m))))))
+
+(define (second-diagonal m)
+  (define (helper i)
+    (if (>= i (length m))
+        '()
+        (cons (nth (nth m i) (- (length m) i 1))
+              (helper (+ i 1)))))
+  (helper 0))
+
 (assert-equal '(1 5 9) (diagonal '((1 2 3) (4 5 6) (7 8 9))))
+(assert-equal '(1 5 9) (diagonal2 '((1 2 3) (4 5 6) (7 8 9))))
+(assert-equal '(3 5 7) (second-diagonal '((1 2 3) (4 5 6) (7 8 9))))
 (assert-equal '(1) (diagonal '((1))))
 (assert-equal '() (diagonal '()))
