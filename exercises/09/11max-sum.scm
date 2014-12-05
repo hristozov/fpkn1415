c9ec52028021ec64ee93f455bba034df6bb381f2
Georgi Hristozov <georgi@forkbomb.nl>
2014-12-05 10:00:51 +0200

    Housekeeping.

---
+++ exercises/09/11max-sum.scm
@@ -0,0 +1,23 @@
+(load "../../lib/scm/unit.scm")
+(load "10list-ways.scm")
+
+(define (max-number tree)
+  (apply max
+         (map (lambda (x) (apply + x)) (list-ways-root tree))))
+
+(define sample-tree
+  (make-tree 1 
+             (make-tree 3
+                        (make-tree 5
+                                   (empty-tree)
+                                   (empty-tree))
+                        (make-tree 6
+                                   (make-tree 8 (empty-tree) (empty-tree))
+                                   (empty-tree)))
+             (make-tree 9
+                        (empty-tree)
+                        (make-tree 7
+                                   (make-tree 2 (empty-tree) (empty-tree))
+                                   (make-tree 1 (empty-tree) (empty-tree))))))
+
+(assert= 19 (max-number sample-tree))
