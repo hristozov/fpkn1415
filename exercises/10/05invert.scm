d79b85c9e62792e072234922211082c2958f7b2a
Georgi Hristozov <georgi@forkbomb.nl>
2014-12-11 16:05:13 +0200

    Revert "Remove a solution."
    This reverts commit e00d3e0eb133d052e6a3fb293065d2811249bc26.

---
+++ exercises/10/05invert.scm
@@ -0,0 +1,18 @@
+(load "../../lib/scm/unit.scm")
+(load "03edges.scm")
+
+(define (invert g)
+  (let ((result (create-graph (vertices g))))
+    (map
+      (lambda (edge)
+        (add-edge! (cadr edge) (car edge) result))
+      (edges g))
+    result))
+
+(define g1 (create-graph '(1 2 3 4)))
+(add-edge! 1 2 g1)
+(add-edge! 2 3 g1)
+(add-edge! 4 3 g1)
+
+(define g2 (invert g1))
+(assert-equal '((1 ()) (2 (1)) (3 (4 2)) (4 ())) g2)
