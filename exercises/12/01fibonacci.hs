85ff565330e1e5306f5c954d66307f625c3f2496
Georgi Hristozov <georgi@forkbomb.nl>
2014-12-15 01:10:04 +0200

    New tasks.

--- exercises/12/01fibonacci.hs
+++ exercises/12/01fibonacci.hs
@@ -1,10 +1,17 @@
 import Unit
 
+-- първи вариант - чрез if
 fibonacci n = if n < 2
   then 1
   else fibonacci (n-1) + fibonacci (n-2)
 
-fibonacci2 n = fib 1 1 1 where 
+-- втори вариант - с pattern matching
+fibonacci2 0 = 1
+fibonacci2 1 = 1
+fibonacci2 n = fibonacci2(n-1) + fibonacci2(n-2)
+
+-- трети вариант - итеративно
+fibonacci3 n = fib 1 1 1 where 
   fib previous current i = 
     if i == n
       then current
@@ -15,5 +22,7 @@ main = do {
   assertEqual 5 (fibonacci 4);
   assertEqual 2 (fibonacci2 2);
   assertEqual 5 (fibonacci2 4);
-  assertEqual 2504730781961 (fibonacci2 60)
+  assertEqual 2 (fibonacci3 2);
+  assertEqual 5 (fibonacci3 4);
+  assertEqual 2504730781961 (fibonacci3 60)
 }
