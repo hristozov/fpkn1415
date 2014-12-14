8103e4894d858a3c1850fe322c0e95c2dd68e478
Georgi Hristozov <georgi@forkbomb.nl>
2014-12-14 12:16:45 +0200

    Add an iterative version.

--- exercises/12/01fibonacci.hs
+++ exercises/12/01fibonacci.hs
@@ -1,11 +1,19 @@
 import Unit
 
-fibonacci :: Int -> Int
 fibonacci n = if n < 2
   then 1
   else fibonacci (n-1) + fibonacci (n-2)
 
+fibonacci2 n = fib 1 1 1 where 
+  fib previous current i = 
+    if i == n
+      then current
+      else fib current (previous + current) (i + 1)
+
 main = do {
   assertEqual 2 (fibonacci 2);
-  assertEqual 5 (fibonacci 4)
+  assertEqual 5 (fibonacci 4);
+  assertEqual 2 (fibonacci2 2);
+  assertEqual 5 (fibonacci2 4);
+  assertEqual 2504730781961 (fibonacci2 60)
 }
