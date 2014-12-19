faa4715575fb671df7438673992c1a1a60557e1d
Georgi Hristozov <georgi@forkbomb.nl>
2014-12-19 09:24:27 +0200

    Housekeeping.

--- exercises/12/01factorial.hs
+++ exercises/12/01factorial.hs
@@ -1,11 +1,15 @@
 import Unit
 
--- fact1 n = if n == 1 then 1 else n * fact1 (n - 1)
+fact n = if n == 1 
+  then 1 
+  else n * fact (n - 1)
 
-fact 1 = 1
-fact n = n * fact (n - 1)
+fact2 1 = 1
+fact2 n = n * fact2 (n - 1)
 
 main = do {
   assertEqual 2 (fact 2);
   assertEqual 24 (fact 4);
+  assertEqual 2 (fact2 2);
+  assertEqual 24 (fact2 4)
 }
--- exercises/12/02count-digits.hs
+++ exercises/12/02count-digits.hs
@@ -1,11 +1,18 @@
 import Unit
 
-countDigits n
+countDigits n = if n < 10
+  then 1
+  else 1 + countDigits (div n 10)
+
+countDigits2 n
   | n < 10 = 1
-  | otherwise = 1 + countDigits (div n 10)
+  | otherwise = 1 + countDigits2 (div n 10)
 
 main = do {
   assertEqual 1 (countDigits 2);
   assertEqual 2 (countDigits 20);
-  assertEqual 6 (countDigits 123456)
+  assertEqual 6 (countDigits 123456);
+  assertEqual 1 (countDigits2 2);
+  assertEqual 2 (countDigits2 20);
+  assertEqual 6 (countDigits2 123456)
 }
