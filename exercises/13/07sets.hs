5ad0d92684d7960b5afe7c68ab0a2dfa3fc2c182
Georgi Hristozov <georgi@forkbomb.nl>
2015-01-08 23:52:44 +0200

    Add "difference".

--- exercises/13/07sets.hs
+++ exercises/13/07sets.hs
@@ -10,6 +10,11 @@ intersection (x:xs) l = if x `elem` l
   then x : xs `intersection` l 
   else xs `intersection` l
 
+difference [] l = []
+difference (x:xs) l = if x `elem` l 
+  then xs `difference` l 
+  else x : xs `difference` l
+
 main = do {
   assertEqual [1] ([1] `union` []);
   assertEqual [1] ([] `union` [1]);
@@ -18,4 +23,8 @@ main = do {
   assertEqual [1, 2, 3] ([1, 2] `union` [2, 3]);
   assertEqual [1] ([1, 2] `intersection` [1, 3]);
   assertEqual [] ([4, 2] `intersection` [1, 3]);
+  assertEqual [] ([1, 2] `difference` [1, 2]);
+  assertEqual [] ([1, 2] `difference` [1, 2, 3]);
+  assertEqual [4] ([1, 2, 4] `difference` [1, 2, 3]);
+  assertEqual [4, 5] ([4, 5] `difference` [1, 2, 3]);
 }
