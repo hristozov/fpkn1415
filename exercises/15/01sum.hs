b87f2f82793473594b04b9a3026c645ff53a4b1d
Georgi Hristozov <georgi@forkbomb.nl>
2015-01-23 00:06:18 +0200

    Init.

---
+++ exercises/15/01sum.hs
@@ -0,0 +1,14 @@
+import Unit
+
+sum' l = foldl (\acc x -> acc + x) 0 l
+sum2 l = foldl (+) 0 l
+sum3 = foldl (+) 0
+sumRange start end = sum' [start..end]
+
+main = do {
+  assertEqual 1 (sum' [1]);
+  assertEqual 6 (sum' [1, 2, 3]);
+  assertEqual 6 (sum2 [1, 2, 3]);
+  assertEqual 6 (sum3 [1, 2, 3]);
+  assertEqual 6 (sumRange 1 3);
+}
